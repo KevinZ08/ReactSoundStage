2e2266490d47ed94f30d5effdcde66cdb658ee3d
KevinZ08 <kevin.z1998@web.de>
2024-03-09 17:49:13 +0100

    FEAT: test

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ var react_1 = require("react");
 var client_1 = require("react-dom/client");
 var App_tsx_1 = require("./App.tsx");
 require("./index.css");
-client_1.default.createRoot(document.getElementById("root")).render(<react_1.default.StrictMode>
+client_1.default.createRoot(document.getElementById("root")).render(
+  <react_1.default.StrictMode>
     <App_tsx_1.default />
-  </react_1.default.StrictMode>);
+  </react_1.default.StrictMode>
+);
