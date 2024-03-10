64ad88f90dfee24e6ca4fa4542c37a90e0b5036f
KevinZ08 <kevin.z1998@web.de>
2024-03-10 15:39:40 +0100

    FEAT: adding NavBar and Pages

---
+++ src/pages/Artist.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Artist() {
+  return (
+    <div className="artist">
+      <h1 className="artist">Artist</h1>
+      <p className="artist">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+        ipsa magnam cupiditate rerum odit sequi non accusantium. Dolorem
+        accusamus, praesentium magnam recusandae facilis possimus voluptatum
+        nulla velit excepturi totam iste..
+      </p>
+    </div>
+  );
+}
+
+export default Artist;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1 className="home">Home</h1>
+      <p className="home">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+        ipsa magnam cupiditate rerum odit sequi non accusantium. Dolorem
+        accusamus, praesentium magnam recusandae facilis possimus voluptatum
+        nulla velit excepturi totam iste..
+      </p>
+    </div>
+  );
+}
+
+export default Home;
